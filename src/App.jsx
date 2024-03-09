ae122fff097e06831fd74137f544b49cd5847aac
momentchan <mingjyunhung@gmail.com>
2024-03-09 17:08:02 +0800

    add debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Physics } from "@react-three/cannon";
+import { Debug, Physics } from "@react-three/cannon";
 import { Environment, OrbitControls } from "@react-three/drei";
 import { Canvas } from '@react-three/fiber'
 import Pointer from "./Pointer";
@@ -26,8 +26,11 @@ export default function App() {
             <spotLight intensity={1} angle={0.2} penumbra={1} position={[30, 30, 30]} castShadow shadow-mapSize={[512, 512]} />
 
             <Physics gravity={[0, 2, 0]} iterations={10}>
-                <Pointer />
-                <Clump />
+                <Debug color="black" scale={1.1}>
+                    <Pointer />
+                    <Clump />
+                    {/* children */}
+                </Debug>
             </Physics>
 
 
