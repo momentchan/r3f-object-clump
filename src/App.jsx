a413a87f72e1a52c5044cc1d54b71d10c231108f
momentchan <mingjyunhung@gmail.com>
2024-03-13 15:25:22 +0800

    Revert "update with rapier physics"
    This reverts commit 796fe549a3c12664b88d8013ad80b5eb71166655.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import { Debug, Physics } from "@react-three/cannon";
 import { Environment, OrbitControls } from "@react-three/drei";
 import { Canvas } from '@react-three/fiber'
+import Pointer from "./Pointer";
 import Clump from "./Clump";
 import { EffectComposer, N8AO, SMAA } from "@react-three/postprocessing";
-import { Physics } from "@react-three/rapier";
-import Pointer from "./r3f-gist/interaction/Pointer";
 
 export default function App() {
     return <>
@@ -13,6 +13,8 @@ export default function App() {
             shadows
             camera={{
                 fov: 35,
+                near: 1,
+                far: 40,
                 position: [0, 0, 15]
             }}>
 
@@ -23,10 +25,12 @@ export default function App() {
             <ambientLight intensity={1.5} />
             <spotLight intensity={1} angle={0.2} penumbra={1} position={[30, 30, 30]} castShadow shadow-mapSize={[512, 512]} />
 
-            <Physics gravity={[0, 0, 0]}>
+            <Physics gravity={[0, 2, 0]} iterations={10}>
                 <Pointer />
                 <Clump />
+                {/* children */}
             </Physics>
+
 
             <EffectComposer disableNormalPass multisampling={0}>
                 <N8AO halfRes color='black' aoRadius={2} intensity={1} aoSamples={6} denoiseSamples={4} />
--- src/Clump.jsx
+++ src/Clump.jsx
@@ -1,12 +1,12 @@
-import * as THREE from 'three'
+import { useSphere } from '@react-three/cannon'
 import { useTexture } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
 import { useControls } from 'leva'
+import * as THREE from 'three'
 import { Outlines } from './r3f-gist/effect/Outlines'
-import { useMemo, useRef } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 import ThreeCustomShaderMaterial from 'three-custom-shader-material'
 import { patchShaders } from 'gl-noise'
-import { InstancedRigidBodies, vec3 } from '@react-three/rapier'
 
 const count = 200
 const rfs = THREE.MathUtils.randFloatSpread
@@ -29,68 +29,59 @@ const shader = {
 }
 
 export default function Clump({ mat = new THREE.Matrix4(), vec = new THREE.Vector3(), ...props }) {
-    const texture = useTexture('eye.png')
+
     const { outlines } = useControls({ outlines: { value: 0.0, step: 0.01, min: 0, max: 0.05 } })
+    const texture = useTexture('ME.png')
+    const [ref, api] = useSphere(() => ({ args: [1], mass: 1, angularDamping: 0.1, linearDamping: 0.65, position: [rfs(20), rfs(20), rfs(20)] }))
+
     const speedBuffer = new THREE.InstancedBufferAttribute(new Float32Array(count), 1);
+    const vectorArrayRef = useRef(Array.from({ length: count }, () => new THREE.Vector3()));
 
-    const rigidBodies = useRef();
-    const mesh = useRef()
-    const instances = useMemo(() => {
-        const instances = [];
-
+    useEffect(() => {
         for (let i = 0; i < count; i++) {
-            instances.push({
-                key: "instance_" + i,
-                position: [rfs(20), rfs(20), rfs(20)],
-                rotation: [Math.random(), Math.random(), Math.random()],
-            })
+            const unsubscribe = api.at(i).velocity.subscribe((v) => (vectorArrayRef.current[i] = v))
         }
-        return instances
-    }, [])
+        return () => {
+            for (let i = 0; i < count; i++) {
+                const unsubscribe = api.at(i).velocity.subscribe((v) => (vectorArrayRef.current[i] = v))
+            }
+        }
+    })
+    const args = useMemo(() => [null, null, count], [count])
 
     useFrame((state, delta) => {
-        if (!rigidBodies.current)
-            return
-
         for (let i = 0; i < count; i++) {
-            const api = rigidBodies.current[i]
-            if (api == null) continue
-            const pos = vec3(api.translation())
-            api.applyImpulse(pos.normalize().multiplyScalar(-1))
-            const vel = api.linvel()
-            const speed = Math.sqrt(vel.x ** 2 + vel.y ** 2 + vel.z ** 2);
+            // Get current sphere
+            ref.current.getMatrixAt(i, mat)
+            // Normalize the position and multiply by a negative force.
+            // This is enough to drive it towards the center-point.
+
+            api.at(i).applyForce(vec.setFromMatrixPosition(mat).normalize().multiplyScalar(-40).toArray(), [0, 0, 0])
+
+            const vel = vectorArrayRef.current[i]
+            var speed = Math.sqrt(vel[0] ** 2 + vel[1] ** 2 + vel[2] ** 2);
+            speed = isNaN(speed) ? 0 : speed
             speedBuffer.setX(i, Math.pow(speed / 20, 2))
         }
-
         speedBuffer.needsUpdate = true
-        mesh.current.geometry.setAttribute('speedBuffer', speedBuffer);
+        ref.current.geometry.setAttribute('speedBuffer', speedBuffer);
     })
 
     return (
-        <InstancedRigidBodies
-            ref={rigidBodies}
-            instances={instances}
-            friction={1}
-            restitution={0}
-            colliders='ball'>
-
-            <instancedMesh
-                ref={mesh}
-                castShadow receiveShadow args={[null, null, count]}>
-                <sphereGeometry args={[1, 32, 32]} />
-                <ThreeCustomShaderMaterial
-                    baseMaterial={THREE.MeshStandardMaterial}
-                    uniforms={{ uTex: texture }}
-                    roughness={0}
-                    envMapIntensity={1}
-                    color='white'
-                    map={texture}
-                    fragmentShader={patchShaders(shader.fragment)}
-                    vertexShader={patchShaders(shader.vertex)}
-                >
-                </ThreeCustomShaderMaterial>
-                <Outlines thickness={outlines} />
-            </instancedMesh>
-        </InstancedRigidBodies>
+        <instancedMesh ref={ref} castShadow receiveShadow args={args}>
+            <sphereGeometry args={[1, 32, 32]} />
+            <ThreeCustomShaderMaterial
+                baseMaterial={THREE.MeshStandardMaterial}
+                uniforms={{ uTex: texture }}
+                roughness={0}
+                envMapIntensity={1}
+                color='white'
+                map={texture}
+                fragmentShader={patchShaders(shader.fragment)}
+                vertexShader={patchShaders(shader.vertex)}
+            >
+            </ThreeCustomShaderMaterial>
+            <Outlines thickness={outlines} />
+        </instancedMesh>
     )
 }
